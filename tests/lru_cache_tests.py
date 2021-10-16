f516183ca30f64651e5212142f81caae3baf9848
Antonio Camacho <jhuno137@gmail.com>
2021-10-16 12:36:44 -0400

    add tests for the doubly linked list: - test head and tail are properly set - set size matches cache size (lookup table)

--- tests/lru_cache_tests.py
+++ tests/lru_cache_tests.py
@@ -1,6 +1,6 @@
 import random
 import unittest
-from cache.lru import LRUCache
+from cache.lru import LRUCache, CacheNode
 
 
 class TestLRUCache(unittest.TestCase):
@@ -62,6 +62,67 @@ class TestLRUCache(unittest.TestCase):
 		cache.delete("3")
 		self.assertEqual(cache.get("3"), None)
 		self.assertEqual(cache._get_size(), 4)
+
+	def test_dll_head_and_tail_are_set_at_get_and_put(self):
+		cache = LRUCache(10)
+		for n in range(5):
+			cache.put(str(n), n)
+			self.assertEqual(cache._head.value, n)
+			if n == 0:
+				# the first time around there is no tail, just head
+				self.assertEqual(cache._tail, None)
+			else:
+				# tail should be set and have the proper value
+				self.assertEqual(cache._tail.value, 0)
+
+		self.assertEqual(cache.get("0"), 0)
+		self.assertEqual(cache._tail.value, 1)
+
+		for n in range(5, 10):
+			# fil the cache up to capacity
+			cache.put(str(n), n)
+			self.assertEqual(cache._head.value, n)
+			self.assertEqual(cache._tail.value, 1)
+
+		cache.put("foo", "bar") 	# when adding a new item, cache needs to evict current current tail
+		self.assertEqual(cache._tail.value, 2)
+
+	def test_dll_has_the_same_size_as_the_cache(self):
+		for m in range(10):
+			capacity = random.randint(1, 1000)
+			cache = LRUCache(capacity)
+			for n in range(capacity):
+				cache.put(str(n), n)
+
+			cache.put("foo", "bar")
+
+			size = self._get_dll_size(cache._head)
+			self.assertEqual(cache._get_size(), size)
+
+			cache.delete("foo")
+			size = self._get_dll_size(cache._head)
+			self.assertEqual(cache._get_size(), size)
+
+			cache.reset()
+			size = self._get_dll_size(cache._head)
+			self.assertEqual(size, 0)
+
+	@staticmethod
+	def _get_dll_size(head: CacheNode) -> int:
+		"""
+		Get the doubly linked list size
+		:param head:
+		:return:
+		"""
+		if head is None:
+			return 0
+		node = head
+		size = 1
+		while node.next is not None:
+			size += 1
+			node = node.next
+
+		return size
 
 
 if __name__ == "__main__":

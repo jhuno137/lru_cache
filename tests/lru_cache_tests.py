a84a83c3aea9c2ab139ba27e60016c997729d306
Antonio Camacho <jhuno137@gmail.com>
2021-10-16 01:19:18 -0400

    increase the number of tests using random numbers

--- tests/lru_cache_tests.py
+++ tests/lru_cache_tests.py
@@ -11,51 +11,54 @@ class TestLRUCache(unittest.TestCase):
 			self.assertRaises(ValueError, LRUCache, capacity)
 
 	def test_cache_does_not_go_over_capacity(self):
-		capacity = random.randint(1, 100)
-		cache = LRUCache(capacity)
-		number_cache_elements = random.randint(100, 200)
-		for n in range(number_cache_elements):
-			cache.put(str(n), n)
-			self.assertLessEqual(cache._get_size(), capacity)
+		for m in range(10):
+			capacity = random.randint(1, 100)
+			cache = LRUCache(capacity)
+			number_cache_elements = random.randint(100, 200)
+			for n in range(number_cache_elements):
+				cache.put(str(n), n)
+				self.assertLessEqual(cache._get_size(), capacity)
 
 	def test_it_gets_the_values(self):
-		capacity = random.randint(10, 1000)
-		cache = LRUCache(capacity)
-		for n in range(capacity):
-			cache.put(str(n), n)
+		for m in range(10):
+			capacity = random.randint(10, 1000)
+			cache = LRUCache(capacity)
+			for n in range(capacity):
+				cache.put(str(n), n)
 
-		for n in range(capacity):
-			self.assertEqual(cache.get(str(n)), n)
+			for n in range(capacity):
+				self.assertEqual(cache.get(str(n)), n)
 
 	def test_it_pops_least_recently_used_cache(self):
-		capacity = random.randint(1, 1000)
-		cache = LRUCache(capacity)
-		for n in range(capacity):
-			# fill cache up to capacity
-			cache.put(str(n), n)
+		for m in range(10):
+			capacity = random.randint(1, 1000)
+			cache = LRUCache(capacity)
+			for n in range(capacity):
+				# fill cache up to capacity
+				cache.put(str(n), n)
 
-		self.assertEqual(cache.get("0"), 0)
-		# Adding one more element to the cache should result in a cache
-		# miss for the first element inserted
-		cache.put("foo", "var")
-		self.assertEqual(cache.get("0"), None)
+			self.assertEqual(cache.get("0"), 0)
+			# Adding one more element to the cache should result in a cache
+			# miss for the first element inserted
+			cache.put("foo", "var")
+			self.assertEqual(cache.get("0"), None)
 
 	def test_it_resets_the_cache(self):
-		capacity = random.randint(1, 1000)
-		cache = LRUCache(capacity)
-		for n in range(capacity):
-			# fill cache up to capacity
-			cache.put(str(n), n)
+		for m in range(10):
+			capacity = random.randint(1, 1000)
+			cache = LRUCache(capacity)
+			for n in range(capacity):
+				# fill cache up to capacity
+				cache.put(str(n), n)
 
-		cache.reset()
-		self.assertEqual(cache._head, None, f"Using capacity of {capacity}")
-		self.assertEqual(cache._tail, None)
-		self.assertEqual(cache._get_size(), 0)
+			cache.reset()
+			self.assertEqual(cache._head, None, f"Using capacity of {capacity}")
+			self.assertEqual(cache._tail, None)
+			self.assertEqual(cache._get_size(), 0)
 
 	def test_it_deletes(self):
-		cache = LRUCache(5)
+		cache = LRUCache(10)
 		for n in range(5):
-			# fill cache up to capacity
 			cache.put(str(n), n)
 
 		self.assertEqual(cache.get("3"), 3)
